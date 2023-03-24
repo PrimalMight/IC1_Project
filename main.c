0723320e560f009e179552fcc256a61b406778da
PrimalMight <xbuzov00@vutbr.cz>
2023-03-24 12:15:44 +0100

    basic overflow attack into bypassing passcheck

--- main.c
+++ main.c
@@ -1,16 +1,29 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-
+#include <string.h>
 
 void vulnerable_function(){
-    char buffer[16];
-    printf("Enter a string: ");
-    gets(buffer);
-    printf("You entered: %s\n", buffer);
+    char password[16];
+    int check_password = 0; // oh my god this is so wrong
 
+    printf("Enter a password: ");
+    gets(password);
+    printf("(delete this afterwards) You entered: %s\n", password);
+
+    if(strcmp(password, "hardcodedpasswordlule")){
+        printf("Try ageeein");
+    }
+    else{
+        printf("u are in");
+        check_password = 1;
+    }
+    if(check_password){
+        // only authenticated should be able to do this (if sudo and is sudoer)
+        
+        system("cat /etc/shadow");
+    }
 
 }
-
 
 int main(){
     vulnerable_function();
--- readme.md
+++ readme.md
@@ -1,1 +1,17 @@
-ahoj svete :)
+### How to
+compile
+```
+gcc -w -fno-stack-protector main.c
+```
+
+---
+
+run (with sudo?)
+```
+python3 exploit.py
+```
+
+---
+
+### Vulnerabilities
+- [x] Buffer overflow into password check bypass
